378d3b1736842e16c47503f5755f7bddeb852a34
rbnrjR <rubanrajr@hotmail.com>
2017-12-06 13:53:45 +0530

    initial working commit with excel sheet read

---
+++ employee.py
@@ -0,0 +1,53 @@
+##########################################################################################################
+#                                                                                                        #
+# filename : employee.py                                                                                 #
+# authors : Rubanraj R(Digital), Aswini K(Digital)                                                       #
+# language : Python3 (numpy,pandas)                                                                      #
+# description : This file reads the par report excel sheet and performs mathematical operations          #
+#               and returns the pandas dataframe of best 5 employees                                     #
+#                                                                                                        #
+##########################################################################################################
+
+import pandas as pd
+from flask import Flask
+from flask_restful import Resource
+import json
+
+class Employees(Resource):
+    def get(self):
+        # reading the par report excel file
+        try:
+            df = pd.read_excel('./data/PAR WMG.xlsx')
+            print('excel file has been read successfully!')
+        except Exception as e:
+            print('error in reading excel - > ', e)
+
+        # passing parameters for filtering employees from par report
+        skill_to_search = 'java'.upper()
+        grade_to_search = 'b2'.upper()
+        location_to_search = 'chennai'.upper()
+
+        # converting the values to uppercase for particular columns in excel dataframe
+        skill = df['PROJECT_ACQUIRED_SKILL'].str.upper()
+        grade = df['GRADE'].str.upper()
+        location = df['LOCATION'].str.upper()
+
+        # performing dataframe operations to filter the employees
+        try:
+            filtered_employees = df[skill.str.contains(skill_to_search)
+                                        & grade.str.contains(grade_to_search)
+                                        & location.str.contains(location_to_search)]
+            best_filtered_employees = filtered_employees[['EMPNO','NAME', 'EMP_EMAIL',
+                                                          'GRADE', 'TOTAL_EXP', 'LOCATION',
+                                                          'ONSITE_OFFSHORE', 'STATUS']].sort_values(['TOTAL_EXP'],
+                                                        axis = 0, ascending = True, na_position = 'last').head(n=5)
+        except Exception as e:
+            print('Error in filtering - > ', e)
+
+        # converting the dataframe to json
+        try:
+            data = best_filtered_employees.to_json(orient = 'records')
+            data_obj = json.loads(data)
+            return data_obj[0]
+        except Exception as e:
+            print('Error in converting dataframe to json - > ', e)
---
+++ server.py
@@ -0,0 +1,21 @@
+##########################################################################################################
+#                                                                                                        #
+# filename : server.py                                                                                   #
+# authors : Rubanraj R(Digital), Aswini K(Digital)                                                       #
+# language : Python3 (Flask Web Framework)                                                               #
+# description : This file is responsible for spinning up the web server of the application and           #
+#               takes care of the routes available in the server                                         #
+#                                                                                                        #
+##########################################################################################################
+
+from flask import Flask
+from flask_restful import Api
+from employee import Employees  # imports Employees class from employee file
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Employees, '/employees') # Route_1
+
+if __name__ == '__main__':
+     app.run(port='5002')  # server running on port 5002
